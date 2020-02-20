eb26b9e6b474a81c39fb756229e008c5cdff253c
Stan Potapov <stan.potapov98@gmail.com>
2020-02-20 18:44:37 -0500

    finished follow cam

--- Assets/Scripts/FollowCam.cs
+++ Assets/Scripts/FollowCam.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FollowCam : MonoBehaviour{
+public class FollowCam : MonoBehaviour {
 
     static public GameObject POI; // The static point of interest
     [Header("Set in Inspector")]
@@ -19,7 +19,7 @@ public class FollowCam : MonoBehaviour{
         if (POI == null) return; // return if there is no poi
         // Get the position of the poi
         Vector3 destination = POI.transform.position;
-        //	Limit	the	X	&	Y	to	minimum	values
+        // Limit the X & Y to minimum values
         destination.x = Mathf.Max(minXY.x, destination.x);
         destination.y = Mathf.Max(minXY.y, destination.y);
         // Interpolate from the current Camera position toward destination
@@ -28,5 +28,7 @@ public class FollowCam : MonoBehaviour{
         destination.z = camZ;
         // Set the camera to the destination
         transform.position = destination;
+        // Set the orthographicSize of the Camera to keep Ground in view
+        Camera.main.orthographicSize = destination.y + 10;
     }
 }
