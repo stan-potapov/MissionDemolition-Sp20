ccbc764fd45ac2ab1b693c19189a3e3e9670868a
Stan Potapov <stan.potapov98@gmail.com>
2020-02-20 21:24:07 -0500

    fixed goal

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -6,7 +6,9 @@ public class Goal : MonoBehaviour{
 
     // A static field accessible by code anywhere
     static public bool goalMet = false;
+
     void OnTriggerEnter(Collider other) {
+        Debug.Log("Goal hit by" + other.gameObject.tag);
         // When the trigger is hit by something
         // Check to see if it's a Projectile
         if (other.gameObject.tag == "Projectile") {
