1ca9e0d04c7d7094bd034c43e77fb14706f074ec
stan-potapov <stan.potapov98@gmail.com>
2020-02-20 23:42:00 -0500

    updated mission demolition script

--- Assets/Scripts/MissionDemolition.cs
+++ Assets/Scripts/MissionDemolition.cs
@@ -59,7 +59,7 @@ public class MissionDemolition : MonoBehaviour {
 
     void UpdateGUI() {
         // Show the data in the GUITexts
-        uitLevel.text = "Level: " + (level + 1) + "of " + levelMax;
+        uitLevel.text = "Level: " + (level + 1) + " of " + levelMax;
         uitShots.text = "Shots Taken: " + shotsTaken;
     }
 
